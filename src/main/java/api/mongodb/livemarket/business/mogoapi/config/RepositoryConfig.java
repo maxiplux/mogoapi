8c6b8be7dcebf5e7b433b2498b0b087f5055ecf0
Juan Mosquera <maxiplux@gmail.com>
2020-06-30 21:38:42 -0400

    improve auditor data

---
+++ src/main/java/api/mongodb/livemarket/business/mogoapi/config/RepositoryConfig.java
@@ -0,0 +1,16 @@
+package api.mongodb.livemarket.business.mogoapi.config;
+
+import api.mongodb.livemarket.business.mogoapi.models.items.Category;
+import api.mongodb.livemarket.business.mogoapi.models.items.Item;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+@Configuration
+public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Item.class);
+        config.exposeIdsFor(Category.class);
+    }
+}
